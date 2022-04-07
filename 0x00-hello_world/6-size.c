bff8c1da0f654a27c24207e82333b5635e95c1cf
-l <drfre08@gmail.com>
2022-04-08 01:49:31 +0300

    typo fixed!

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ long long int l;
 char charType;
 float floatType;
 
-printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(charType));
-printf("Size of an int: %lu byte(s) \n", (unsigned long)sizeof(intType));
-printf("ize of a long int: %lu byte (s) \n", (unsigned long) sizeof(longType));
-printf("ize of a long long int: %lu byte(s) \n", (unsigned long) sizeof(l));
-printf("Size of a float: %lu byte (s) \n", (unsigned long)sizeof(floatType));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charType));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intType));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(longType));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(l));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
 
 return (0);
 }
