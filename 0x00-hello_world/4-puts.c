96f111124c257d730115d80ff9f3008dd29b0b7a
-l <drfre08@gmail.com>
2022-04-08 00:57:12 +0300

    added a comment for return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 /**
  * main - entry point
  *
- * return always 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
+/* End of program returns 0 */
 return (0);
 }
