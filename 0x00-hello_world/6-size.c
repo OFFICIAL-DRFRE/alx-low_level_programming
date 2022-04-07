8d7243435ea2462db8b03f5095a82680830d82ed
-l <drfre08@gmail.com>
2022-04-08 01:33:03 +0300

    cleaned the betty style issues

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,18 @@
  * Return: Always 0 (sucess)
  */
 
-int main (void)
+int main(void)
 {
 int intType;
 long int longType;
-long long int longLongIntType;
+long long int lliType;
 char charType;
 float floatType;
-printf( "Size of a char: %lu byte(s) \n", (unsigned long)sizeof(charType));
+
+printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(charType));
 printf("Size of an int: %lu byte(s) \n", (unsigned long)sizeof (intType));
 printf("ize of a long int: %lu byte (s) \n", (unsigned long) sizeof (longType));
-printf("ize of a long long int: %lu byte(s) \n", (unsigned long) sizeof (longLongIntType) );
+printf("ize of a long long int: %lu byte(s) \n", (unsigned long) sizeof (lliType);
 printf("Size of a float: %lu byte (s) \n", (unsigned long)sizeof (floatType));
 return (0);
 }
