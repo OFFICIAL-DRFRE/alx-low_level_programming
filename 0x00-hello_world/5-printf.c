c139d746de9053826050eb90fd158a2396a1f17c
-l <drfre08@gmail.com>
2022-04-08 01:02:47 +0300

    wrote a printf c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Aways 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+
+/* End of the program */
+return (0);
+}
