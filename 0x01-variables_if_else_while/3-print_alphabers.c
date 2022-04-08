de8e318ffb3780cafaddf31f045a554e4d1e5176
-l <drfre08@gmail.com>
2022-04-08 14:27:26 +0300

    print the alphabets in capital

---
+++ 0x01-variables_if_else_while/3-print_alphabers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+return (0);
+}
