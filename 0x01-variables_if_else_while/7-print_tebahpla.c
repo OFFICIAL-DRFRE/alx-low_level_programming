d5ba473844e396bde19c9c87b775c1e8eef62f11
-l <drfre08@gmail.com>
2022-04-08 15:20:46 +0300

    reverse the alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+i#include <stdio.h>
+
+/**
+ * main - entry point reverse print the aplhabets,
+ *        only using putchar and without char variables.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; num >= 'a'; i--)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
