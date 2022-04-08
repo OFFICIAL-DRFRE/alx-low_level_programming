3e407fdba2ede58f6ab9ff3e5d5ff6f77ffbf11f
OFFICIAL-DRFRE <drfre08@gmail.com>
2022-04-08 10:10:57 +0300

    positive or negative number checker

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#incldue<stdio.h>
+#include<stdlib.h>
+/**
+ * main - point of entry
+ *
+ * Return: Always 0 (sucess)
+ */
+int main (void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* Check if the random number is positive or negative*/
+	if ( n > 0 )
+		printf("%d is positive\n", n);
+	else if ( n == 0 )
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+return (0);
+}
+	
