c882a76b38961c2ca523c6161646d3f834677fd7
-l <drfre08@gmail.com>
2022-04-13 13:08:07 +0300

    last bonus

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - 98 seq of fibonacci
+ * Return: 0
+ */
+int main(void)
+{
+	int f1 = 0, f2 = 1, i, n=98, next;
+
+	for (i = 1; i < n; i++) 
+	{
+		printf("%d ,", f2);
+		next = f1 + f2;
+		f1 = f2;
+		f2 = next;
+
+	}
+return (0):
+}
