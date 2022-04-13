d8b362aa13473902ff47a1c7e5bb7513c98732c0
FREHIWOT <100209006+OFFICIAL-DRFRE@users.noreply.github.com>
2022-04-13 21:59:17 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
+
+#define MAXULI9 10000000000
+
 /**
- * main - 98 seq of fibonacci
- * Return: 0
+ * main - prints the first 100 Fibonacci numbers, starting with 1 and 2,
+ * followed by a new line, without using long long, arrays or dynamic
+ * allocation
+ *
+ * Return: 0 on success
  */
 int main(void)
 {
-	unsigned long f1b1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
 	int i;
+	unsigned long onebeforefirst = 0, onebeforesecond = 0;
+	unsigned long twobeforefirst = 0, twobeforesecond = 0;
+	unsigned long currentfirst = 0, currentsecond = 0;
 
-	for (i = 0; i < 92; i++)
+	onebeforesecond = 2;
+	twobeforesecond = 1;
+	currentsecond = onebeforesecond + twobeforesecond;
+
+	printf("%lu", twobeforesecond);
+	printf(", %lu", onebeforesecond);
+	printf(", %lu", currentsecond);
+
+	for (i = 3; i < 98; i++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-		fib1 = fib2;
-		fib2 = sum;
-	}
+		twobeforefirst = onebeforefirst;
+		twobeforesecond = onebeforesecond;
 
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
+		onebeforefirst = currentfirst;
+		onebeforesecond = currentsecond;
 
-	for (i = 93; count < 99; i++)
-	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
+		currentfirst = twobeforefirst + onebeforefirst;
+		currentsecond = twobeforesecond + onebeforesecond;
+
+		if (currentsecond >= MAXULI9)
 		{
-			half1 += 1;
-			half2 %= 10000000000;
+			currentfirst++;
+			currentsecond -= MAXULI9;
 		}
 
-		printf("%lu%lu", half1, half2);
-		if (cont != 98)
-		printf(", ");
-
+		if (currentfirst != 0)
+			printf(", %lu%010lu", currentfirst, currentsecond);
+		else
+			printf(", %lu", currentsecond);
 	}
-return (0);
+	printf("\n");
+	return (0);
 }
