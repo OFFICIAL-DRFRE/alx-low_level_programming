7e013346a18c5d47f917c33dc4d27ffc2c19d635
-l <drfre08@gmail.com>
2022-04-15 17:25:29 +0300

    changed it to char 0 - 9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
  * _isdigit - checks for 0-9 digits
  * @c: character 
  *
  * Return: 0 or 1
  */
-
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
