b521cec0e4f17eb574699fbb9f73bcec5288cb90
-l <drfre08@gmail.com>
2022-04-18 22:32:30 +0300

    CHANGED THE ALGORITHM

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,35 @@
 #include "main.h"
+
 /**
- * print_rev - Prints a string in reverse.
- * @s: string to be reserved.
+ * _strlen - get length of a string
+ *
+ * @s: string to get length of
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string to print in reverse
+ *
+ * Return: void
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int i;
 
-	while (s[i++])
-		len++;
-
-	for (i = len - 1; i >= 0; i--)
+	for (i = _strlen(s) - 1; i >= 0; i--)
 		_putchar(s[i]);
-
 	_putchar('\n');
 }
-
