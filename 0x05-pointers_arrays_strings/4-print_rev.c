c74229f27f66c0db7493235b11b2a75b880b3e7b
-l <drfre08@gmail.com>
2022-04-18 22:38:40 +0300

    update the count method

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,35 +1,18 @@
 #include "main.h"
-
 /**
- * _strlen - get length of a string
- *
- * @s: string to get length of
- *
- * Return: length of string
- */
-size_t _strlen(char *s)
-{
-	size_t i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
-
-	return (i);
-}
-
-/**
- * print_rev - prints a string in reverse
- *
- * @s: string to print in reverse
- *
- * Return: void
+ * print_rev - Prints a string in reverse.
+ * @s: string to be reserved.
  */
 void print_rev(char *s)
 {
-	int i;
+	int len = 0, i;
 
-	for (i = _strlen(s) - 1; i >= 0; i--)
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
+
 	_putchar('\n');
 }
+
