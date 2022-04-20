a3d85b0e19b5c9217dbe6b102f2f5c323b439d7d
-l <drfre08@gmail.com>
2022-04-21 00:07:46 +0300

    removed the - sign with 45

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,10 +2,10 @@
 
 /**
  * Print_number: print the numbers with putchar
- * You can only use _putchar function to print
-* You are not allowed to use long
-* You are not allowed to use arrays or pointers
-* You are not allowed to hard-code special values
+ *only use _putchar to print
+* not allowed to use long
+* not allowed to use arrays/pointers
+* not allowed to hard-code special values
 * Return: Always 0
 */
 
@@ -15,8 +15,8 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
-		i *= -1;
+		_putchar(45);
+		i = -i;
 	}
 	if (i / 10)
 	{
