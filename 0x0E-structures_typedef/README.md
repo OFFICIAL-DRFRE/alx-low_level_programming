31618cd32eb8a8a9e18bd98ead86dd63a7dfeecc
-l <drfre08@gmail.com>
2022-05-09 22:49:20 +0300

    Struct assignment

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+=======================================================================
+This project will help us learn how to use and declare structures on C
+======================================================================
+project by Frehiwot H.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,39 @@
+#ifndef FREY_H
+#define FREY_H
+
+/**
+ * struct dog - this structure store few dog's characteristics
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description:  this structure store few dog's characteristics
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * struct dog_t - this structure store few dog's characteristics
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description:  this structure store few dog's characteristics
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* FREY_H */
